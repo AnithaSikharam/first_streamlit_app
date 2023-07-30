304ed4b78351eda326d72b4853dd3b496d7d71a8
AnithaSikharam <140787111+AnithaSikharam@users.noreply.github.com>
2023-07-30 15:24:47 +0530

    Update streamlit_app.py
    change title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 
 streamlit.header(' Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
@@ -19,3 +19,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 
 streamlit.dataframe(fruits_to_show)
+
+
