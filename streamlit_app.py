28d161ec0c99c7f7fa2dc3cf5e7d7799c16a2712
AnithaSikharam <140787111+AnithaSikharam@users.noreply.github.com>
2023-07-31 22:33:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,4 @@ def get_fruityvice_data(this_fruit_choice):
      return fruityvice_normalized
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
