16376edc33820ad512a80d0a8a61647ebd50666e
AnithaSikharam <140787111+AnithaSikharam@users.noreply.github.com>
2023-07-31 23:41:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,10 +30,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
-
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
