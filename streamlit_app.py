d49190ba4bae164e8964c68d792ef03c8ad34698
AnithaSikharam <140787111+AnithaSikharam@users.noreply.github.com>
2023-07-31 16:14:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,9 @@ streamlit.write('Thanks for adding', add_my_fruit)
 
 my_cur.execute("Insert into fruit_load_list values ('test')")
 
+streamlit.write('What fruit would you like to add?')
+
+
 
 
 
