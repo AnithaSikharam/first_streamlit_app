092552f53c4a5aecabb6e0d7f9913074c7db5cb7
AnithaSikharam <140787111+AnithaSikharam@users.noreply.github.com>
2023-07-31 22:45:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,13 @@ def get_fruityvice_data(this_fruit_choice):
 	
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-   back_from_function = get_fruityvice_data('kiwi')
-	streamlit.dataframe(back_from_function)
+try:
+   this_fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not this_fruit_choice:
+    streamlit.error("Please select a fruit to get information:")
+   else:
+    back_from_function = get_fruityvice_data(this_fruit_choice)
+    streamlit.dataframe(back_from_function)
+    
+    except URLError as e:
+   streamlit.error()
