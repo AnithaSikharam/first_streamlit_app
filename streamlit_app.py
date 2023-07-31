6e16789285a9f6ef7e8d4a617f41f531ecfb6967
AnithaSikharam <140787111+AnithaSikharam@users.noreply.github.com>
2023-07-31 15:43:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,13 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+my_cur.execute("delete from fruit_load_list values ('jackfruit')")
+
+
+
 streamlit.write('Thanks for adding', add_my_fruit)
 my_cur.execute("Insert into fruit_load_list values ('test')")
+
 
 
 
