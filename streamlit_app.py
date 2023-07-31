22f9b5d0f1e83a4372ca18eef11f99668b64d064
AnithaSikharam <140787111+AnithaSikharam@users.noreply.github.com>
2023-07-31 22:11:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,4 +32,3 @@ try:
     streamlit.error("Please select a fruit to get information:")
    else:
     back_from_function = get_fruityvice_data(this_fruit_choice)
-    streamlit.dataframe(back_from_function)
