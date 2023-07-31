864a1cfab2a54aacef27846b64f0760d45b288b3
AnithaSikharam <140787111+AnithaSikharam@users.noreply.github.com>
2023-07-31 17:31:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,11 +48,11 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-
+#New Section to display Fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-If not fruit_choice:
+if not fruit_choice:
  streamlit.error("Please select a fruit to get information:")
 else:
  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
